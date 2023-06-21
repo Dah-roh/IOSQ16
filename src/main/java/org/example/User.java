35738cf87f6319f78bc856dad63d27fb7313ba38
daro-decagon <gadibiao@decagonhq.com>
2023-06-21 11:06:43 +0100

    initial commit

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Objects;
+@Data
+@Builder
+@AllArgsConstructor
+public class User {
+    private String name;
+    private Role Role;
+    private String stack;
+
+}
